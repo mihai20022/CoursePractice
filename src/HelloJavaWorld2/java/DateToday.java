dca24a1a2dc1701dac9fc9e5679d7a6e7c8f6825
TulbureMIhai <mihaitulbure01@gmail.com>
2022-01-30 17:14:44 +0200

    3

---
+++ src/HelloJavaWorld2/java/DateToday.java
@@ -0,0 +1,11 @@
+package HelloJavaWorld2.java;
+
+import java.time.LocalTime;
+
+    class Main {
+    public static void main(String[] args){
+        LocalTime Time = LocalTime.now();
+        System.out.println("Local time is " + Time);
+
+    }
+}
