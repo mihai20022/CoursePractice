0ff0513a3c41338a386b8fbb8f9430fa92a45e6e
TulbureMIhai <mihaitulbure01@gmail.com>
2022-01-30 16:49:14 +0200

    2

---
+++ src/HelloJavaWorld1/java/Covid19.java
@@ -0,0 +1,11 @@
+package HelloJavaWorld1.java;
+
+public class Covid19 {
+    public static void main(String[] args){
+        int age = 30;
+        String time = "01:30:21";
+        System.out.println("Age is  " + age + " at " + time);
+
+
+    }
+}
