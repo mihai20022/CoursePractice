5e6d19fc37286958bb8b09c75e2bb5492a9b7976
TulbureMIhai <mihaitulbure01@gmail.com>
2022-02-01 11:37:26 +0200

    5

---
+++ src/HomeworkLesson4/java/Variables.java
@@ -0,0 +1,19 @@
+package HomeworkLesson4.java;
+
+public class Variables {
+
+    public static void main(String[] args){
+        long a = 7_811_080_855L;
+        int b = 55_235_358;
+        char c = 'A';
+        boolean d = true;
+        final float pi = 3.14f;
+
+        System.out.println("Current World Population - " + a);
+        System.out.println("Cars Produced This Year - " + b);
+        System.out.println("First Letter In Alphabet - " + c);
+        System.out.println("Java Is Amazing - " + d);
+        System.out.println("Pi - " + pi);
+
+    }
+}
