b45e697c1f77165b928adbd457b840bce270644c
TulbureMIhai <mihaitulbure01@gmail.com>
2022-02-08 17:45:55 +0200

    4314

--- src/HomeworkLesson6/java/Color.java
+++ src/HomeworkLesson6/java/Color.java
@@ -1,47 +1,58 @@
 package HomeworkLesson6.java;
 import java.util.Scanner;
 
-/*public class Color {
+public class Color {
     public static void main(String[] args){
 
+        do{
+
         Scanner code = new Scanner(System.in);
-        System.out.println("Select and type the Color Code to get the color: \n Examples: 100; 101; 102; 103.");
+        System.out.println("Select and type the Color Code to get the color: \n Examples: 100; 101; 102;");
         int  colorCode = code.nextInt();
+        String color ;
 
-         colorCode = (colorCode == 100)?
-                            (colorCode == 101 )?
-                                    (colorCode == 102 )?
-                                            (colorCode == 103 )?
-                                            getSpec1Disc():
-                                    getSpec2Disc():
-                            getSpec3Disc():
-                     getSpec4Disc():
-                 getSpec5Disc();
+          color = (colorCode == 100)? getSpec1Disc():
+                            (colorCode == 101 )?getSpec2Disc():
+                                    (colorCode == 102 )?getSpec3Disc():
+                                            
+                     getSpec4Disc();
 
 
 
-        System.out.println(colorCode);
-    }
 
 
-    static void  getSpec1Disc(){
-        System.out.println("red");
-    }
-    static void getSpec2Disc(){
-        System.out.println("Yellow");
 
-    }
-    static void getSpec3Disc(){
-        System.out.println("Green");
 
-    }
-    static void getSpec4Disc(){
-        System.out.println("Black");
+        System.out.println(color);
+            System.out.println("You want to try again. Press\n 1 for stop\n 2 for continue");
+            String stop = code.next();
+            if (stop.equals("1")) {
+                break;
 
-    }
-    static void getSpec5Disc(){
-        System.out.println("Black");
+            } else
+                System.out.println("You want to try again. Enter color code");
+            continue;
+
+        }while (true);
 
     }
 
-}*/
+
+    static String  getSpec1Disc(){
+        return "Yellow";
+    }
+    static String getSpec2Disc(){
+        return"Green";
+
+    }
+    static String getSpec3Disc(){
+        return"Red";
+
+    }
+    static String getSpec4Disc(){
+        return"Invalid";
+
+    }
+
+
+}
